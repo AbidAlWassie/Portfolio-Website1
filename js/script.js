c8f19f09b2a89b80f16257d8b6736b2975016de1
Abid Al-Wasi <abidalwasi@outlook.com>
2020-08-01 00:29:19 +0600

    updated alongside everything

--- js/script.js
+++ js/script.js
@@ -1,5 +1,52 @@
 window.onload = function () {
 
+  function smoothScroll(target, duration) {
+    var target = document.querySelector(target);
+    var targetPosition = target.getBoundingClientRect().top;
+    var startPosition = window.pageYOffset;
+    var distance = targetPosition - startPosition;
+    var startTime = null;
+
+    function animation(currentTime) {
+      if(startTime === null) startTime = currentTime;
+      var timeElapsed = currentTime - startTime;
+      var run = ease(timeElapsed, startPosition, distance, duration);
+      window.scrollTo(0, run);
+      if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    function ease(t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return c / 2 * t * t + b;
+      t--;
+      return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation);
+  }
+  
+  // distinations config
+  var skillsLocation = "#skillsSect";
+  // var 
+  
+
+  // buttons config
+  var mouseBtn = document.getElementById("mouseBtn");
+
+  var portfolioBtn = document.getElementById("portfolioBtn");
+  var workBtn = document.getElementById("workBtn");
+  var aboutBtn = document.getElementById("aboutBtn");
+  var servicesBtn = document.getElementById("servicesBtn");
+  var skillsBtn = document.getElementById("skillsBtn");
+  var contactBtn = document.getElementById("contactBtn");
+  
+  mouseBtn.addEventListener("click", getToSkillsSect);
+
+  
+  function getToSkillsSect() {
+    smoothScroll(skillsLocation, 500);
+  }
+
 
   const menuBtn = document.querySelector(".menu-btn");
 
