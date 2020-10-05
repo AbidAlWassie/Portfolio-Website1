a1fab86dfcf5324c31fc4cdb645dc370dc365c2f
Abid Al-Wasi <abidalwasi@outlook.com>
2020-10-05 11:01:31 +0600

    updated alongside everything

--- js/script.js
+++ js/script.js
@@ -76,7 +76,7 @@ window.onload = function () {
   
         var hash = this.hash;
   
-        var target = $(hash).offset().top-73.99;
+        var target = $(hash).offset().top-150;
         $('html, body').animate({
           scrollTop: target
         }, 500, function(){
