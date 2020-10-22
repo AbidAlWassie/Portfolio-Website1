abb77f2e09ab6e5275b56b6ab7a013f653c01845
Abid Al-Wasi <abidalwasi@outlook.com>
2020-10-23 02:54:02 +0600

    updated alongside everything

--- js/script.js
+++ js/script.js
@@ -108,24 +108,24 @@ window.onload = function () {
     var $window = $(window);
 
 
-    if ($window.width() <= 768) {
-      context = 'small';
-    } else if (768 < $window.width() < 992) {
-      context = 'medium';
-    } else {
-      context = 'large';
-    }
-
-    $(window).resize(function() {
-      if(($window.width() <= 768) && (context != 'small')) {
-        //refresh the page
-        location.reload();
-      } else if ((768 < $window.width()  < 992) && (context != 'medium')) {
-        location.reload();
-      } else if (context != 'large') {
-        location.reload();
-      }
-    });
+    // if ($window.width() <= 768) {
+    //   context = 'small';
+    // } else if (768 < $window.width() < 992) {
+    //   context = 'medium';
+    // } else {
+    //   context = 'large';
+    // }
+    //
+    // $(window).resize(function() {
+    //   if(($window.width() <= 768) && (context != 'small')) {
+    //     //refresh the page
+    //     location.reload();
+    //   } else if ((768 < $window.width()  < 992) && (context != 'medium')) {
+    //     location.reload();
+    //   } else if (context != 'large') {
+    //     location.reload();
+    //   }
+    // });
 
     });
 
